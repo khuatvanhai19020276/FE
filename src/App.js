18f01df5a09a0a5edc32d32d4e4a4a11e4d34651
khuathai308 <haivankhuat3082001@gmail.com>
2022-09-25 21:08:18 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component { 
+  constructor() { 
+    super(); 
+    this.state = { 
+      users: []
+        } 
+      } 
+  async componentDidMount() { 
+    await fetch('http://localhost:5000/books')
+    .then(res => res.json()) 
+    .then((data) => { 
+      this.setState({ users: data.books })
+    }) 
+  
+    .catch(console.log) 
+  } 
+ 
+  
+  renderUsers = () => {
+    return this.state.users.map((data, index) => {
+      return ( 
+      <tr key= {data.id}>
+        <td>{data._id}</td>
+        <td>{data.name}</td>
+        <td>{data.author}</td>
+        <td>{data.publishYear}</td>
+      </tr>
+     ); 
+      })
+  }
+  
+  render() { 
+    return (
+      <div className='App'>
+        <div className='container'>
+          <h1 className='text-center'> List</h1>
+      <div className="col-md-4">
+      <select className="custom-select" >
+      <option value="all">all</option>
+        <option value="hai">hai</option>
+        <option value="khuat">khuat</option>
+        <option value="nguyen">nguyennnnnnnnnnnnnnnnn</option>
+      </select>
+      </div>
+
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Author</th>
+                <th>PublishYear</th>
+              </tr>
+            </thead>
+            <tbody> 
+
+              {this.renderUsers()}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+    
+    
+    
+
+
+    
